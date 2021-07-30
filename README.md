15ba5106fc6206fc8cbfe76dac1b62a71a6b1fb2
Francisco Tamayo <lebe.java@gmail.com>
2021-07-29 22:36:25 -0500

    Hook example implemented with MaterialUI

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). An example using Hooks to add a new static item to table.
 
 ## Available Scripts
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import React, { useState } from 'react';
 import "./App.css";
 import styled from "styled-components";
 
 import { NavBar } from "./components/NavBar";
 
-const data = [
+import PropTypes from "prop-types";
+import clsx from "clsx";
+
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+
+import { withStyles } from "@material-ui/core/styles";
+
+// We can inject some CSS into the DOM.
+const styles = {
+  root: {
+    background: "#ff7661",
+    borderRadius: 3,
+    border: 0,
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    marginTop: "16px",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+  },
+};
+
+const defaultData = [
   {
     id: 0,
     date: "22/04/2021",
@@ -58,7 +81,7 @@ const data = [
 
 const Table = styled.table`
   width: 80%;
-  padding: 64px;
+  padding: 16px 16px 64px 64px;
   text-align: left;
 `;
 
@@ -98,7 +121,7 @@ const Amount = styled.p`
 //   color: green;
 // `;
 
-function App() {
+function App(props) {
   // const amountCell = (transaction) => {
   //   // switch(transaction.type)
   //   return transaction.type === "expense" ? (
@@ -108,41 +131,77 @@ function App() {
   //   );
   // };
 
+  const [data, setData] = useState(defaultData);
+
+  const { classes, children, className, ...other } = props;
+
+  const add = () => {
+    const newData = {
+      id: 0,
+      date: "29/07/2021",
+      name: "Example",
+      category: "example",
+      amount: 123,
+      type: "income",
+    }
+
+    setData(data => [...data, newData])
+  }
+
   return (
-    <div className='layout'>
-      <NavBar />
-      <Table>
-        <thead>
-          <tr>
-            <HeadCell>Date</HeadCell>
-            <HeadCell>Name</HeadCell>
-            <HeadCell>Category</HeadCell>
-            <HeadCell>Amount</HeadCell>
-            <HeadCell>Actions</HeadCell>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((transaction) => {
-            /* console.log("transaction", transaction); */
-            return (
-              <tr key={transaction.id}>
-                <TableCell>{transaction.date}</TableCell>
-                <TableCell>{transaction.name}</TableCell>
-                <TableCell>{transaction.category}</TableCell>
-                <TableCell>
-                  <Amount type={transaction.type}>{transaction.amount}</Amount>
-                </TableCell>
-
-                {/* {amountCell(transaction)} */}
-
-                <TableCell>Edit/Delete</TableCell>
+    <div className="layout">
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <NavBar />
+        </Grid>
+        <Grid item xs={9}>
+          <Button className={clsx(classes.root, className)} {...other} onClick={add}>
+            ADD
+          </Button>
+          <Table>
+            <thead>
+              <tr>
+                <HeadCell>Date</HeadCell>
+                <HeadCell>Name</HeadCell>
+                <HeadCell>Category</HeadCell>
+                <HeadCell>Amount</HeadCell>
+                <HeadCell>Actions</HeadCell>
               </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+            </thead>
+            <tbody>
+              {data.map((transaction) => {
+                /* console.log("transaction", transaction); */
+                return (
+                  <tr key={transaction.id}>
+                    <TableCell>{transaction.date}</TableCell>
+                    <TableCell>{transaction.name}</TableCell>
+                    <TableCell>{transaction.category}</TableCell>
+                    <TableCell>
+                      <Amount type={transaction.type}>
+                        {transaction.amount}
+                      </Amount>
+                    </TableCell>
+
+                    {/* {amountCell(transaction)} */}
+
+                    <TableCell>Edit/Delete</TableCell>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Grid>
+      </Grid>
     </div>
   );
 }
 
-export default App;
+App.propTypes = {
+  children: PropTypes.node,
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
+};
+
+export default withStyles(styles)(App);
+
+//export default App;
